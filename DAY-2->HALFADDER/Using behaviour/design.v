7459cb2c25bfec1150174eff50b7eb2ad97bf9b8
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-09-19 15:45:28 +0530

    Delete DAY-2->HALFADDER directory

--- DAY-2->HALFADDER/Using behaviour/design.v
+++
@@ -1,7 +0,0 @@
-/* used xor gate for sum and and gate for carry by behaviour model*/
-module halfadder(input a,b,output reg sum,carry);
-  always @*begin
-     sum=a^b;
-     carry=a&b;
-  end
-endmodule
