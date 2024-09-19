9308637ab4e015da879d4a3a10f92c48dc43f22c
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-09-19 16:35:08 +0530

    Update testbench.v

--- Day-3->Full adder/Using halfadder/testbench.v
+++ Day-3->Full adder/Using halfadder/testbench.v
@@ -5,10 +5,15 @@ wire s_out, c_out;
 full_adder inst_a(a, b, c_in, s_out, c_out);
 initial begin
     $monitor("Time=%0t a=%b b=%b c_in=%b s_out=%b c_out=%b", $time, a, b, c_in, s_out, c_out);
-    #1; a = 1; b = 0; c_in = 0;
-    #1; a = 0; b = 1; c_in = 0;
-    #1; a = 1; b = 1; c_in = 0;
-    #1; a = 0; b = 0; c_in = 1;
-    #1; a = 1; b = 1; c_in = 1;
-end
+    #5 a=0;b=0;cin=0;
+    #5 a=0;b=0;cin=1;
+    #5 a=0;b=1;cin=0;
+    #5 a=0;b=1;cin=1;
+    #5 a=1;b=0;cin=0;
+    #5 a=1;b=0;cin=1;
+    #5 a=1;b=1;cin=0;
+    #5 a=1;b=1;cin=1;
+    #5
+    $finish;
+  end
 endmodule
