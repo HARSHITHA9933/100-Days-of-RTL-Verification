4fe96639ed3c6789f3edf567549c924d9083e054
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-09-22 14:49:31 +0530

    Update testbench.v

--- Day-6->Ripplecarryadder/testbench.v
+++ Day-6->Ripplecarryadder/testbench.v
@@ -4,8 +4,11 @@ output reg S,C;
   ripplecarryadder dut(A,B,cin,S,C);
   initial begin
     $monitor("Time=%0t A=%b B=%b cin=%b S=%b C=%b",A,B,cin,S,C);
-    #1 A=4'b0010; B=4'b0001;
-    #1 A=4'b1000; B=4'b1100;
+    #1 A=4'b0110; B=4'b0110;
+    #1 A=4'b0111; B=4'b1110;
+    #1 A=4'b0010;B=4'b1001;
+    #1 
+    $finish;
   end
 endmodule
     
