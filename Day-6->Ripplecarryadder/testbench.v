d8fc857f19e0ea60983504b0f4fb89bd94fdb4bc
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-09-22 14:33:40 +0530

    Update testbench.v

--- Day-6->Ripplecarryadder/testbench.v
+++ Day-6->Ripplecarryadder/testbench.v
@@ -1,1 +1,11 @@
-
+module ripplecarryadder_tb(A,B,cin,S,C);
+input wire A,B,cin;
+output reg S,C;
+  ripplecarryadder dut(A,B,cin,S,C);
+  initial begin
+    $monitor("Time=%0t A=%b B=%b cin=%b S=%b C=%b",A,B,cin,S,C);
+    #1 A=4'b0010; B=4'b0001;
+    #1 A=4'b1000; B=4'b1100;
+  end
+endmodule
+    
