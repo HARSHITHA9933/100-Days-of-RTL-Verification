a9bb4510eeed0366b19b9ff88a519205cfb8263c
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-09-26 17:50:24 +0530

    Update testbench.v

--- Day-10->Comparator/one bit/testbench.v
+++ Day-10->Comparator/one bit/testbench.v
@@ -1,1 +1,16 @@
+`include "comp.v"
+module comp_test(a,b,x,y,z);
+ input wire x,y;
+ output reg a,b;
+comp inst(.a(a), .b(b), x(x), y(y), .z(2));
+initial begin 
+  $monitor("Time=%0t a=%0b b=%0b x=%0b y=%0b", $time,a,b,x,y,z); 
+#10 x=0;y=0;
+  #10 x=0;y=1;
+  #10 x=1;y=0;
+  #10 x=1;y=1;
+  #10;
+  $finish;
+end
+endmodule
 
