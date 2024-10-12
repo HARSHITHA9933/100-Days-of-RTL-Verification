a6ae22d01f7b74bf700a9c4e24574c3863ad8839
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-10-12 15:46:23 +0530

    Update testbench.v

--- Day-25->SISO and SIPO registers/SISO/testbench.v
+++ Day-25->SISO and SIPO registers/SISO/testbench.v
@@ -8,7 +8,7 @@ clk=1'b0;
 forever #5 clk=-clk;
 end
 initial begin
-$monitor("Time=>%0t clk=%bd%b q=%b", $time,clk,d,q);
+  $monitor("Time=>%0t clk=%b d=%b q=%b", $time,clk,d,q);
 #5 d=0;
 #10 d=1;
 #10 d=0;
