34fdd6ab3360ef469797f9abaad44399891b5adf
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-11-14 12:56:43 +0530

    Update interface.sv

--- Day-54->TB to verify RIPPLE CARRY ADDER/interface.sv
+++ Day-54->TB to verify RIPPLE CARRY ADDER/interface.sv
@@ -1,1 +1,11 @@
-
+interface intf(); 
+  logic [3:0]a;
+  logic[3:0] b;
+  logic cin;
+  logic [3:0]sum;
+  logic [3:0]carry;
+  
+  modport WR_DRV_MP (output a,b,cin);
+  modport WR_MON_MP (input a,b,cin);
+  modport RD_MON_MP ( input sum,carry);
+endinterface
