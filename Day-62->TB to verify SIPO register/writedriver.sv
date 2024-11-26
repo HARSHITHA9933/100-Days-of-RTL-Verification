cd4df586110198e525afbffb8460e631a013e77f
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-11-26 17:16:37 +0530

    Update writedriver.sv

--- Day-62->TB to verify SIPO register/writedriver.sv
+++ Day-62->TB to verify SIPO register/writedriver.sv
@@ -1,1 +1,21 @@
+class driver;
+  virtual intf.WR_DRV_MP wr_drv_inf;
+  transaction data2drv;
+  mailbox #(transaction) gen2wr;
+
+  function new(virtual intf.WR_DRV_MP wr_drv_inf, mailbox #(transaction) gen2wr);
+    this.wr_drv_inf = wr_drv_inf;
+    this.gen2wr = gen2wr;
+  endfunction
+
+  virtual task drive();
+    forever begin
+      gen2wr.get(data2drv); // Get data from generator
+      wr_drv_inf.d <= data2drv.d; // Drive the data
+      wr_drv_inf.clk <= data2drv.clk; // Align the clock
+      data2drv.display("DRV"); // Display the data being driven
+      #5; // Small delay to allow propagation
+    end
+  endtask
+endclass
 
