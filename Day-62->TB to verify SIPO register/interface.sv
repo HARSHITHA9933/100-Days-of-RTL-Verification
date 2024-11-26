17a3222465bd9f2eeb2c2c797f686e6fdf0a90c9
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-11-26 17:11:12 +0530

    Update interface.sv

--- Day-62->TB to verify SIPO register/interface.sv
+++ Day-62->TB to verify SIPO register/interface.sv
@@ -1,1 +1,10 @@
 
+interface intf;
+  logic clk;
+  logic d;
+  logic [3:0] q;
+
+  modport WR_DRV_MP (output d,clk);
+  modport WR_MON_MP (input d, clk);
+  modport RD_MON_MP (input q, clk);
+endinterface
