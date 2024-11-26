afcd46392c4e2a4c1819fd23262215f9207d55ed
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-11-26 17:14:38 +0530

    Update scoreboard.sv

--- Day-62->TB to verify SIPO register/scoreboard.sv
+++ Day-62->TB to verify SIPO register/scoreboard.sv
@@ -1,1 +1,25 @@
+class scoreboard;
+  transaction rm_data, rcvdata;
+  mailbox #(transaction) ref2scr, rdmon2sc;
+
+  function new(mailbox #(transaction) ref2scr, mailbox #(transaction) rdmon2sc);
+    this.ref2scr = ref2scr;
+    this.rdmon2sc = rdmon2sc;
+  endfunction
+
+  virtual task start();
+    forever begin
+      ref2scr.get(rm_data); // Get expected data from reference model
+      rdmon2sc.get(rcvdata); // Get actual data from read monitor
+      check(rcvdata); // Compare the data
+    end
+  endtask
+
+  virtual task check(transaction rc_data);
+    if (rm_data.q === rc_data.q)
+      $display("MATCH SUCCESS: Expected = %b, Received = %b", rm_data.q, rc_data.q);
+    else
+      $display("MISMATCH: Expected = %b, Received = %b", rm_data.q, rc_data.q);
+  endtask
+endclass
 
