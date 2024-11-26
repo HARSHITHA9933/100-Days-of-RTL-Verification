56452786dfbfc4f2f9bece04a3e6d979531c0bb2
HARSHITHA9933 <peddagorlaharshitha@gmail.com>
2024-11-26 17:16:09 +0530

    Update transaction.sv

--- Day-62->TB to verify SIPO register/transaction.sv
+++ Day-62->TB to verify SIPO register/transaction.sv
@@ -1,1 +1,9 @@
+class transaction;
+   rand bit d;
+  bit [3:0]q;
+  rand bit clk;
+    function void display(string label);
+      $display("Time:%0t [%s]: q:%0b clk:%0b d:%0b ", $time, label,q,clk,d);
+    endfunction
+endclass
 
